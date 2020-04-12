cd06e58128cefc5d97bae1f511dd4994dda504fd
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 13:07:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -15,16 +15,19 @@ end
 #Loop over the array and output each element
 def output_array(array)
   i=0
-  while i<array.length
+  while i<array.length do
   puts array[i]
   i+=1
+  end
 end
 
+#
 def return_string_array(array)
-  
-  
-  
-  
+  i=0
+  while i<array.length do
+  puts array[i]
+  i+=1
+  end
 end
 
 
