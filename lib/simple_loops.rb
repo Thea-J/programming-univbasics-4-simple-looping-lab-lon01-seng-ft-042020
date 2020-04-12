96927670fc86489983c4d70a6ef6754b270c11ef
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 13:02:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,9 +5,11 @@ def loop_message_five_times(string)
   end
 end
 
-#
+#The number of times the message is output is equal to the provided integer
 def loop_message_n_times(string, integer)
-  
+   integer.times do 
+    puts string
+  end
 end
 
 def output_array
